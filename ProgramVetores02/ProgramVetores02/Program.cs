bf8d1471ac955e6101a0b5a7ac4e4e642c727c2c
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-18 22:58:59 -0300

    Exercício02
    Faça um programa que leia N números inteiros e armazene-os em um vetor. Em seguida, mostre na tela:
    - todos os números pares
    - a quantidade de números pares.
    
    Exemplo:
    Entrada                            Saída
    6                                      8  2  14  20
    8 2 11 14 13 20               4

---
+++ ProgramVetores02/ProgramVetores02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ProgramVetores02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int[] A = new int[N];
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < N; i++)
+            {
+                A[i] = int.Parse(valores[i]);
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                if (A[i] % 2 == 0)
+                {
+                    Console.Write(A[i] + " ");
+                }
+            }
+
+            Console.WriteLine();
+
+            int contPar = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                if (A[i] % 2 == 0)
+                {
+                    contPar++;
+                }
+            }
+
+            Console.WriteLine(contPar);
+        }
+    }
+}
