ab4ee6fd037005511430a3ebbfc2ab8d4f8de33a
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-19 10:22:56 -0300

    Exercício04
    Fazer um programa para ler um vetor de N números reais. Em seguida, mostrar na tela a média aritmética de todos
    elementos. Depois mostrar todos os elementos do vetor que estejam abaixo da média.
    
    Exemplo:
    Entrada                                      Saída
    4                                                12.125
    10.0  15.5  13.2  9.8                   10.0
                                                      9.8

---
+++ ProgramVetores04/ProgramVetores04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProgramVetores04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[N];
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < N; i++)
+            {
+                vet[i] = double.Parse(valores[i], CultureInfo.InvariantCulture);
+            }
+
+            double media = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                media += vet[i] / N;
+            }
+            Console.WriteLine(media.ToString("F3", CultureInfo.InvariantCulture));
+
+            for (int i = 0; i < N; i++)
+            {
+                if (vet[i] < media)
+                {
+                    Console.WriteLine(vet[i].ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
