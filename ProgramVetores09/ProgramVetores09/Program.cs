9ac40b0c7048f449b701f4898982362035fe8111
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-19 14:29:41 -0300

    Exercício09
    Um comerciante deseja fazer o levantamento do lucro das mercadorias que ele comercializa. Para isto, mandou digitar
    um conjunto de N mercadorias, cada uma contendo nome, preço de compra e preço de venda das mesmas. Fazer um
    programa que leia tais dados e determine e escreva quantas mercadorias proporcionaram:
    
    - lucro< 10%;
    - 10% ≤ lucro ≤ 20%;
    - lucro> 20%.
    
    Determine e escreva também o valor total de compra e de venda de todas as mercadorias, assim como o
    lucro total.
    
    Exemplo:
    Entrada                                               Saída
    4                                                         Lucro abaixo de 10%: 1
    Feijao  10.00  11.00                            Lucro entre 10% e 20%: 2
    Arroz  12.00  12.80                             Lucro acima de 20%: 1
    Oleo  5.00  5.70                                  Valor total de compra: 30.00
    Sal  3.00  4.00                                     Valor total de venda: 33.50
                                                               Lucro total: 3.50

---
+++ ProgramVetores09/ProgramVetores09/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+
+namespace ProgramVetores09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            string[] nome = new string[N];
+            double[] precoCompra = new double[N];
+            double[] precoVenda = new double[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+                nome[i] = valores[0];
+                precoCompra[i] = double.Parse(valores[1], CultureInfo.InvariantCulture);
+                precoVenda[i] = double.Parse(valores[2], CultureInfo.InvariantCulture);
+            }
+
+
+            int lucroAbaixo10 = 0;
+            int lucroEntre10E20 = 0;
+            int lucroAcima20 = 0;
+            for (int i = 0; i < N; i++)
+            {
+                double lucro = precoVenda[i] - precoCompra[i];
+
+                double porcentagemLucro = lucro / precoCompra[i] * 100.0;
+
+                if (porcentagemLucro < 10.0)
+                {
+                    lucroAbaixo10++;
+                } else if (porcentagemLucro <= 20.0 )
+                {
+                    lucroEntre10E20++;
+                } else
+                {
+                    lucroAcima20++;
+                }
+            }
+            Console.WriteLine("Lucro abaixo de 10%: " + lucroAbaixo10);
+            Console.WriteLine("Lucro entre 10% e 20%: " + lucroEntre10E20);
+            Console.WriteLine("Lucro acima de 20%: " + lucroAcima20);
+
+
+            double totalCompra = 0.0;
+            double totalVenda = 0.0;
+            for (int i = 0; i < N; i++)
+            {
+                totalCompra += precoCompra[i];
+                totalVenda += precoVenda[i];
+            }
+            double totalLucro = totalVenda - totalCompra;
+
+            Console.WriteLine("Valor total de compra: " + totalCompra.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor total de venda: " + totalVenda.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Lucro total: " + totalLucro.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
