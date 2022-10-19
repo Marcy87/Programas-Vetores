e6ad2d2baa834fe95569c5ac1e25fb7fe5703add
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-19 10:55:53 -0300

    Exercício05
    Fazer um programa para ler um vetor de N números inteiros. Em seguida, mostrar na tela a média aritmética somente
    dos números pares lidos.
    
    Exemplo:
    Entrada                              Saída
    6                                        11.0
    8 2 11 14 13 20

---
+++ ProgramVetores05/ProgramVetores05/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProgramVetores05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int[] vet = new int[N];
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < N; i++)
+            {
+                vet[i] = int.Parse(valores[i]);
+            }
+
+            int soma = 0;
+            int divisor = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                if (vet[i] % 2 == 0)
+                {
+                    soma += vet[i];
+                    divisor++;
+
+                }
+            }
+
+            if (divisor == 0)
+            {
+                Console.WriteLine("Nao havis nenhum número par");
+            } else
+            {
+                double media = (double)soma / divisor;
+                Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
