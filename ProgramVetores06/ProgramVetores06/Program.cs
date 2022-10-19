910adb3af4f2fc064fbfa7523e7e961b06d3f45d
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-19 11:22:38 -0300

    Exercício06
    Fazer um programa para ler um conjunto de nomes de pessoas e suas respectivas idades. Depois, mostrar na tela o nome
    da pessoa mais velha.
    
    Exemplo:
    Entrada                         Saída
    5                                   Pessoa mais velha: Carlos
    Joao 16
    Maria 21
    Teresa 15
    Carlos 23
    Paulo 17

---
+++ ProgramVetores06/ProgramVetores06/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ProgramVetores05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            string[] nome = new string[N];
+            int[] idade = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+                nome[i] = valores[0];
+                idade[i] = int.Parse(valores[1]);
+            }
+
+            int maiorIdade = idade[0];
+            int posicaoMaiorIdade = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                if (idade[i] > maiorIdade)
+                {
+                    maiorIdade = idade[i];
+                    posicaoMaiorIdade = i;
+                }
+            }
+
+            Console.WriteLine("Pessoa mais velha: " + nome[posicaoMaiorIdade]);
+        }
+    }
+}
